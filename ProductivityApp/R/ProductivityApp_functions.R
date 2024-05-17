16a141f2fe6c82e99af0f353f8b66a306375fd83
elena-sek <elena.sekaninova@gmail.com>
2024-05-17 15:44:02 +0200

    Added function descriptions
    Added descriptions to the 3 functions that I have so far.

--- ProductivityApp/R/ProductivityApp_functions.R
+++ ProductivityApp/R/ProductivityApp_functions.R
@@ -1,3 +1,10 @@
+#' @title Create an Empty Task File
+#' @description Creates an empty csv file with a default name tasks.csv. The file has 5 columns named task, duration, importance, urgency, and enjoyability.
+#' @param filename string with a default value "tasks.csv"
+#' @return csv file is created
+#' @examples
+#' # Create an empty task csv file in your current working directory
+#' create_empty_task_csv()
 #' @export
 create_empty_task_csv <- function(filename = "tasks.csv") {
   # Creating an empty data frame with specified column names
@@ -13,6 +20,17 @@ create_empty_task_csv <- function(filename = "tasks.csv") {
   write.csv(data, file = filename, row.names = FALSE)
 }
 
+#' @title Add Task 
+#' @description Adds a new task with its variables to the tasks.csv file. 
+#' @param task character, name of the task you want to add
+#' @param duration integer, duration of the task in minutes
+#' @param importance integer (1-4), how important the task is
+#' @param urgency integer(1-4), how urgent the task is
+#' @param enjoyability integer(1-3), how enjoyable the task is
+#' @return the tasks.csv file is updated
+#' @examples
+#' # Add a task to your task file
+#' add_task(task = "Programming", duration = 180, importance = 4, urgency = 3, enjoyability = 2)
 #' @export
 add_task <- function(task, duration, importance, urgency, enjoyability) {
   # Load tasks' data from the existing csv file
@@ -31,6 +49,13 @@ add_task <- function(task, duration, importance, urgency, enjoyability) {
   write.csv(tasks_data, file = "tasks.csv", row.names = FALSE)
 }
 
+#' @title Generate an Eisenhower Matrix
+#' @description Creates an Eisenhower matrix based on a file with tasks.
+#' @param filename character, name of the task csv file, default is "tasks.csv"
+#' @return the Eisenhower matrix
+#' @examples
+#' # Generate an Eisenhower matrix, assuming you have "tasks.csv" file in your working directory
+#' generate_eisenhower_matrix()
 #' @export
 generate_eisenhower_matrix <- function(filename = "tasks.csv"){
   # NEEDS IMPROVEMENT
