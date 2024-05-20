4a7d51c856def76e777558a50f366fc7a6a149ff
elena-sek <elena.sekaninova@gmail.com>
2024-05-20 15:17:13 +0200

    Updated 3 functions and their descriptions
    Deleted function create_empty_task_csv and created create_empty_task_df function instead. Updated add_task and generate_eisenhower_matrix functions and their descriptions.

--- ProductivityApp/R/ProductivityApp_functions.R
+++ ProductivityApp/R/ProductivityApp_functions.R
@@ -1,80 +1,84 @@
-#' @title Create an Empty Task File
-#' @description Creates an empty csv file with a default name tasks.csv. The file has 5 columns named task, duration, importance, urgency, and enjoyability.
-#' @param filename string with a default value "tasks.csv"
-#' @return csv file is created
+#' @title Create an Empty Task Data Frame
+#' @description Creates an empty task data frame. The data frame has 5 columns named Task, Duration, Importance, Urgency, and Enjoyability. It does not automatically convert strings into factors.
+#' @return An empty data frame with columns Task, Duration, Importance, Urgency, and Enjoyability.
 #' @examples
-#' # Create an empty task csv file in your current working directory
-#' create_empty_task_csv()
+#' # Create an empty task data frame 
+#' create_empty_task_df()
 #' @export
-create_empty_task_csv <- function(filename = "tasks.csv") {
+create_empty_task_df <- function() {
   # Creating an empty data frame with specified column names
-  data <- data.frame(
-    task = character(),
-    duration = numeric(),
-    importance = numeric(),
-    urgency = numeric(),
-    enjoyability = numeric()
+  data.frame(
+    Task = character(),
+    Duration = numeric(),
+    Importance = numeric(),
+    Urgency = numeric(),
+    Enjoyability = numeric(),
+    stringsAsFactors = FALSE
   )
-  
-  # Writing the data frame to a CSV file
-  write.csv(data, file = filename, row.names = FALSE)
 }
 
 #' @title Add Task 
-#' @description Adds a new task with its variables to the tasks.csv file. 
+#' @description Adds a new task with its variables to the task data frame with columns Task, Duration, Importance, Urgency, and Enjoyability.
+#' @param df name of the task data frame that will be updated
 #' @param task character, name of the task you want to add
 #' @param duration integer, duration of the task in minutes
 #' @param importance integer (1-4), how important the task is
 #' @param urgency integer(1-4), how urgent the task is
 #' @param enjoyability integer(1-3), how enjoyable the task is
-#' @return the tasks.csv file is updated
+#' @param shiny Boolean, whether this function will be used with shiny's reactive dataframe, default is TRUE 
+#' @return the updated task data frame
 #' @examples
-#' # Add a task to your task file
-#' add_task(task = "Programming", duration = 180, importance = 4, urgency = 3, enjoyability = 2)
+#' # Add a task to an example data frame
+#' example_df = data.frame(Task = c("Meeting preparation", "Programming", "Watch lecture recording"),Duration = c(30, 60, 75),Importance = c(4, 3, 3),Urgency = c(4, 2, 3),Enjoyability = c(1, 3, 2),stringsAsFactors = FALSE)
+#' add_task(df = example_df, task = "Programming", duration = 180, importance = 4, urgency = 3, enjoyability = 2, shiny = FALSE)
 #' @export
-add_task <- function(task, duration, importance, urgency, enjoyability) {
-  # Load tasks' data from the existing csv file
-  tasks_data <- read.csv("tasks.csv")
-  
-  # Add new task data to the csv file
+add_task <- function(df, task, duration, importance, urgency, enjoyability, shiny = TRUE) {
+  # Add new task data to the task data frame
   new_row <- data.frame(
-    task = task,
-    duration = duration,
-    importance = importance,
-    urgency = urgency,
-    enjoyability = enjoyability
+    Task = task,
+    Duration = duration,
+    Importance = importance,
+    Urgency = urgency,
+    Enjoyability = enjoyability,
+    stringsAsFactors = FALSE
   )
-  
-  tasks_data <- rbind(tasks_data, new_row)
-  write.csv(tasks_data, file = "tasks.csv", row.names = FALSE)
+  if (shiny == TRUE){
+    df(rbind(df(), new_row))
+  } else {
+    rbind(df, new_row)
+  }
 }
 
 #' @title Generate an Eisenhower Matrix
-#' @description Creates an Eisenhower matrix based on a file with tasks.
-#' @param filename character, name of the task csv file, default is "tasks.csv"
+#' @description Creates an Eisenhower matrix based on a data frame with tasks.
+#' @param df name of the task data frame
 #' @return the Eisenhower matrix
 #' @examples
-#' # Generate an Eisenhower matrix, assuming you have "tasks.csv" file in your working directory
-#' generate_eisenhower_matrix()
+#' # Generate an Eisenhower matrix based on an example data frame
+#' example_df = data.frame(Task = c("Meeting preparation", "Programming", "Watch lecture recording"),Duration = c(30, 60, 75),Importance = c(4, 3, 3),Urgency = c(4, 2, 3),Enjoyability = c(1, 3, 2),stringsAsFactors = FALSE)
+#' generate_eisenhower_matrix(df = example_df)
 #' @export
-generate_eisenhower_matrix <- function(filename = "tasks.csv"){
-  # NEEDS IMPROVEMENT
-  tasks_data <- read.csv(filename)
-  
-  plot <- ggplot2::ggplot(tasks_data, ggplot2::aes(x = urgency, y = importance, label = task)) +
+generate_eisenhower_matrix <- function(df){
+  ggplot2::ggplot(df, ggplot2::aes(x = Urgency, y = Importance, label = Task)) +
     ggplot2::geom_point(color = "blue", size = 3) +  # Points representing tasks
-    ggplot2::geom_text(size = 3, hjust = -0.2, vjust = -0.5) +  # Labels for tasks
-    ggplot2::xlim(0, 4) + ggplot2::ylim(0, 4) +  # Set limits for x and y axes
-    ggplot2::labs(title = "Eisenhower Matrix", x = "Urgency", y = "Importance") +  # Axis labels and plot title
-    ggplot2::theme_minimal()  # Minimal theme for the plot
-  
-  print(plot)
+    ggplot2::geom_text(size = 5, hjust = 0.5, vjust = 1.5) +  # Labels for tasks
+    ggplot2::xlim(0.5, 4.5) + ggplot2::ylim(0.5, 4.5) +  # Set limits for x and y axes
+    ggplot2::labs(title = "Eisenhower Matrix", x = "Urgency", y = "Importance") +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(
+      plot.title = ggplot2::element_text(size = 20, face = "bold", hjust = 0.5),  # Title size
+      axis.title = ggplot2::element_text(size = 16),  # Axis labels size
+      axis.text = ggplot2::element_text(size = 16)  # Axis tick labels size
+    ) +
+    ggplot2::geom_hline(yintercept = 2.5, linetype = "solid", color = "black", linewidth = 1) +  # Horizontal line
+    ggplot2::geom_vline(xintercept = 2.5, linetype = "solid", color = "black", linewidth = 1)  # Vertical line
 }
 
 # update_preferences <- function(free_time = 360, start_task_under_5 = TRUE, start_enjoyable = FALSE) {
-#   # HERE MAYBE CREATE A CSV FILE WITH PREFERENCES ? OR JUST A DATAFRAME
+#   # not sure if I need this function
 # }
 # 
 # generate_todolist <- function(){
+#   # still need to develop this function
 #   # generate todolist based on preferences 
 # }
